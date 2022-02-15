4c0c34b314cbaa51fd47b4bc9a4fbc60a0a3b462
Nathanael Gray <ngray@forefrontweb.com>
2022-02-15 17:59:53 -0500

    finished the hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 import CountDown from "../components/CountDown";
 import Footer from "../components/Footer";
 import "../scss/entry.scss";
@@ -9,6 +9,7 @@ const IndexPage = () => {
 	return (
 		<>
 			<Header />
+			<Hero />
 			{CountDown()}
 			<Footer />
 		</>
